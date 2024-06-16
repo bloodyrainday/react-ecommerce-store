5e4a1968015f6cdfbf83271bc593a2e43644fa70
bloodyrainday <bloodyrainday@gmail.com>
2024-06-16 16:42:50 +0300

    init: start a new project

---
+++ frontend/src/pages/Shop.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../components/Banner";
+
+type Props = {};
+
+const Shop = (props: Props) => {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+};
+
+export default Shop;
