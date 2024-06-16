5acc990d74391a3864f0f54ceac173706ef62c8e
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2024-06-16 22:35:34 +0300

    feat: add Popular component

--- frontend/src/components/Item/index.tsx
+++ frontend/src/components/Item/index.tsx
@@ -5,14 +5,19 @@ import "./style.scss";
 type Props = {
   image: string;
   name: string;
+  new_price: number;
+  old_price: number;
 };
 
-const Item = ({ image, name }: Props) => {
+const Item = ({ image, name, new_price, old_price }: Props) => {
   return (
     <div className="item">
       <img src={image} alt="item-img" />
       <p>{name}</p>
-      <div className="item__prices"></div>
+      <div className="item__prices">
+        <span className="item__price-new">{new_price}</span>
+        <span className="item__price-old">{old_price}</span>
+      </div>
     </div>
   );
 };
---
+++ frontend/src/components/Popular/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.scss";
+
+import data_product from "../../assets/data";
+
+type Props = {};
+
+const Popular = (props: Props) => {
+  return <div>index</div>;
+};
+
+export default Popular;
