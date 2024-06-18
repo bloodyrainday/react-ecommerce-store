7fed9a7a6645e1458ea7881c57204bbe489c1e13
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2024-06-18 15:32:41 +0300

    feat: add html structure to the Login component

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 
+import "./styles/Login.scss";
+
 type Props = {};
 
 const Login = (props: Props) => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="login__container">
+        <h2>Sign Up</h2>
+
+        <div className="login__fields">
+          <input type="text" placeholder="name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+        </div>
+        <button>continue</button>
+        <p className="login__have-account">
+          already have an account? <span>login here</span>
+        </p>
+
+        <div className="login__agree">
+          <label htmlFor="">
+            <input type="checkbox" name="" id="" />
+            by continuing, I agree to the terms of use & privacy policy
+          </label>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
