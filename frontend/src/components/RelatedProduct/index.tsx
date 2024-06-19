a4583515dba80792d83d9b59fc13030f87f57ec2
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2024-06-19 23:39:13 +0300

    feat: add RelatedProduct component

---
+++ frontend/src/components/RelatedProduct/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./style.scss";
+
+import data_product from "../../assets/data";
+import Item from "../Item";
+
+type Props = {};
+
+const RelatedProduct = (props: Props) => {
+  return (
+    <div className="relatedproduct">
+      <h2>Related Products</h2>
+      <hr />
+      <div className="relatedproduct__item">
+        {data_product.map((item) => {
+          return <Item item={item} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default RelatedProduct;
